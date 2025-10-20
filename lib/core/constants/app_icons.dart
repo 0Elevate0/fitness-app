0307815336a3ef1ee668d3a8747d0441e5a942bc
MahmoodEbrahim <m7kmod7k2003@gmail.com>
2025-10-20 17:28:34 +0300

    add icons on app icon file

--- lib/core/constants/app_icons.dart
+++ lib/core/constants/app_icons.dart
@@ -2,4 +2,8 @@ abstract final class AppIcons {
   static const String email = "assets/icons/email.svg";
   static const String passwordLock = "assets/icons/password_lock.svg";
   static const String back = "assets/icons/back.svg";
+  static const String homeIcon = "assets/icons/home-icon.png";
+  static const String gymIcon = "assets/icons/gym-icon.png";
+  static const String profileIcon = "assets/icons/profile-icon.png";
+  static const String chatIcon = "assets/icons/chat-ai-icon.png";
 }
