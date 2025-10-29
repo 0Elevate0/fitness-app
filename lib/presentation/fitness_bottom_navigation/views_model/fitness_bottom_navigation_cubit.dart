ca5510b55a9ed3273a1ae04a5ee51df9250d75ac
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-10-29 11:45:22 +0300

    feat(FIT1-14): fixing workout bottom navigation bug

--- lib/presentation/fitness_bottom_navigation/views_model/fitness_bottom_navigation_cubit.dart
+++ lib/presentation/fitness_bottom_navigation/views_model/fitness_bottom_navigation_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:fitness_app/presentation/fitness_bottom_navigation/views_model/fitness_bottom_navigation_intent.dart';
 import 'package:fitness_app/presentation/fitness_bottom_navigation/views_model/fitness_bottom_navigation_state.dart';
+import 'package:fitness_app/presentation/work_out/views/work_out_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
@@ -17,6 +18,11 @@ class FitnessBottomNavigationCubit extends Cubit<FitnessBottomNavigationState> {
   }
 
   void _changeIndex({required int index, Widget? changedTap}) {
+    _checkWorkoutScreenNavigation(index: index, changedTap: changedTap);
+    emit(state.copyWith(currentIndex: index));
+  }
+
+  void _checkWorkoutScreenNavigation({required int index, Widget? changedTap}) {
     if (changedTap != null) {
       final tempTaps = List.generate(
         state.taps.length,
@@ -26,7 +32,15 @@ class FitnessBottomNavigationCubit extends Cubit<FitnessBottomNavigationState> {
       tempTaps.insert(index, changedTap);
       emit(state.copyWith(currentIndex: index, taps: tempTaps));
       return;
+    } else if (index == 2) {
+      final tempTaps = List.generate(
+        state.taps.length,
+        (index) => state.taps[index],
+      );
+      tempTaps.removeAt(index);
+      tempTaps.insert(index, const WorkOutView());
+      emit(state.copyWith(currentIndex: index, taps: tempTaps));
+      return;
     }
-    emit(state.copyWith(currentIndex: index));
   }
 }
