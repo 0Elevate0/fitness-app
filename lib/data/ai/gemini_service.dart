1b3851f11038a8f3eb813cbdeeb4de2d02568b7c
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-11-11 16:05:01 +0200

    refactor(FIT1-17): using .env for api keys

--- lib/data/ai/gemini_service.dart
+++ lib/data/ai/gemini_service.dart
@@ -1,14 +1,12 @@
-import 'package:fitness_app/core/constants/api_keys.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:injectable/injectable.dart';
 
 @lazySingleton
 class GeminiService {
   Future<String> sendMessage(String userMessage) async {
-    final model = GenerativeModel(
-      model: 'gemini-2.5-flash',
-      apiKey: ApiKeys.gemini,
-    );
+    final geminiKey = dotenv.get('gemini_key', fallback: "");
+    final model = GenerativeModel(model: 'gemini-2.5-flash', apiKey: geminiKey);
     try {
       final content = [Content.text(userMessage)];
       final response = await model.generateContent(content);
